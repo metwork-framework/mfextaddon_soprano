9db33fca849a91b3545a94c5e936b5cb8314f3cf
metworkbot <metworkbot@metwork-framework.org>
2021-05-17 15:39:44 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/integration_tests.sh
+++ .github/workflows/integration_tests.sh
@@ -15,6 +15,9 @@ echo -e "gpgcheck=0\n\enabled=1\n\metadata_expire=0\n" >>/etc/yum.repos.d/metwor
     cat liste_dep | sort -u > liste_dep2
     yum -y install `cat liste_dep2`
 
+
+    yum -y localinstall `ls -lrt /private/metwork_addons/continuous_integration/rpms/${BRANCH}/${OS_VERSION}/metwork-mfext-layer-python3_radartools* | tail -1 | awk '{print $NF}'`
+
     rm /etc/yum.repos.d/metwork.repo
     yum clean all
     yum -y localinstall ./rpms/metwork-mfext*.rpm
