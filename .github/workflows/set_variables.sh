79406af11eb6a8cd8b4a7b842ee481a00a9af7f7
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-05-19 15:15:24 +0200

    build: update workflow

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -1,38 +1,103 @@
 #!/bin/bash
 
+#set -eu
+set -x
 
-
-
-
-set -eu
-
-if test "centos6" = ""; then
-    echo "ERROR: OS env is empty"
+if test "${OS_VERSION:-}" = ""; then
+    echo "ERROR: OS_VERSION env is empty"
     exit 1
 fi
 
+TAG=
+DEP_BRANCH=
+TARGET_DIR=
+DEP_DIR=
+
 case "${GITHUB_EVENT_NAME}" in
     repository_dispatch)
-        BRANCH=${PAYLOAD_BRANCH};;
+        B=${PAYLOAD_BRANCH};;
+    pull_request)
+        case "${GITHUB_BASE_REF}" in
+            master | integration | experimental* | release_* | ci* | pci*)
+                B=${GITHUB_BASE_REF};;
+            *)
+                B=null;
+        esac;;
     push)
         case "${GITHUB_REF}" in
+            refs/tags/v*)
+                B=`git branch -a --contains "${GITHUB_REF}" | grep remotes | grep release_ | cut -d"/" -f3`;;
             refs/heads/*)
-                BRANCH=${GITHUB_REF#refs/heads/};;
+                B=${GITHUB_REF#refs/heads/};;
             *)
-                BRANCH=null;
+                B=null;
         esac;;
 esac
-if [ -z ${BRANCH} ]; then
-  BRANCH=null
+if [ -z ${B} ]; then
+  B=null
 fi
-TAG_LATEST=""
+if [ "${GITHUB_EVENT_NAME}" != "repository_dispatch" ]; then
+    case "${GITHUB_REF}" in
+        refs/heads/experimental* | refs/heads/master | refs/heads/release_*)
+            DEP_BRANCH=${B}
+            DEP_DIR=${B##release_}
+            TARGET_DIR=${B##release_};;
+        refs/heads/integration | refs/heads/ci* | refs/heads/pci*)
+            DEP_BRANCH=integration
+            DEP_DIR=master
+            TARGET_DIR=master;;
+        refs/tags/v*)
+            TAG=${GITHUB_REF#refs/tags/}
+            DEP_BRANCH=${B}
+            DEP_DIR=${B##release_}
+            TARGET_DIR=${B##release_};;
+        refs/pull/*)
+case "${B}" in
+                integration | ci* | pci*)
+                    DEP_BRANCH=integration
+                    DEP_DIR=master
+                    TARGET_DIR=master;;
+                *)
+                    DEP_BRANCH=${B}
+                    DEP_DIR=${B##release_}
+                    TARGET_DIR=${B##release_};;
+            esac;;
+    esac
+else
+    # GITHUB_REF is always "refs/heads/master" in this case (repository_dispatch)
+    case "${B}" in
+        master | experimental* | release_*)
+            DEP_BRANCH=${B}
+            DEP_DIR=${B##release_}
+            TARGET_DIR=${B##release_};;
+        integration | ci* | pci*)
+            DEP_BRANCH=integration
+            DEP_DIR=master
+            TARGET_DIR=master;;
+    esac
+fi
+
+if [ -z ${TAG} ]; then
+  CI=continuous_integration
+else
+  CI=releases
+fi
 
 
-TAG_BRANCH="metwork/mfxxx-centos6-testimage:${BRANCH}"
-if test "${BRANCH}" = "master"; then
-    TAG_LATEST="metwork/mfxxx-centos6-testimage:latest"
-fi 
+    
+        
+        
+    
 
-echo "::set-output name=branch::${BRANCH}"
-echo "::set-output name=tag_branch::${TAG_BRANCH}"
-echo "::set-output name=tag_latest::${TAG_LATEST}"
+
+echo "::set-output name=branch::${B}"
+echo "::set-output name=tag::${TAG}"
+echo "::set-output name=dep_branch::${DEP_BRANCH}"
+echo "::set-output name=target_dir::${TARGET_DIR}"
+echo "::set-output name=dep_dir::${DEP_DIR}"
+echo "::set-output name=buildimage::metwork/mfxxx-${OS_VERSION}-buildimage:${DEP_BRANCH}"
+echo "::set-output name=testimage::metwork/mfxxx-${OS_VERSION}-testimage:${DEP_BRANCH}"
+echo "::set-output name=buildlog_dir::/pub/metwork/${CI}/buildlogs/${B}/mfextaddon_soprano/${OS_VERSION}/${GITHUB_RUN_NUMBER}"
+
+echo "::set-output name=rpm_dir::/private/metwork_addons/${CI}/rpms/${B}/${OS_VERSION}"
+echo "::set-output name=doc_dir::/private/metwork_addons/${CI}/docs/${B}/mfextaddon_soprano"
