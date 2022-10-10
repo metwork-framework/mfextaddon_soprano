c4a6ef45cde6db404c74e448c3563bb5d5f11b7b
metworkbot <metworkbot@metwork-framework.org>
2022-10-10 11:30:21 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -116,3 +116,6 @@ echo "::set-output name=skip_dispatch::${SKIP_DISPATCH}"
 
 echo "::set-output name=rpm_dir::/private/metwork_addons/${CI}/rpms/${B}/${OS_VERSION}"
 echo "::set-output name=doc_dir::/private/metwork_addons/${CI}/docs/${B}/mfextaddon_soprano"
+
+echo "::set-output name=nexus_dir::https://artifacts.cloudmf.dev/repository/rpm-releases-metwork-addons/${B}/${OS_VERSION}/";
+
