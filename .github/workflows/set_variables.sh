912c9990a3cca4074630228d46916cbcfa262ebb
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-10-10 13:56:39 +0000

    build: upload rpms to nexus dev

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -116,3 +116,13 @@ echo "::set-output name=skip_dispatch::${SKIP_DISPATCH}"
 
 echo "::set-output name=rpm_dir::/private/metwork_addons/${CI}/rpms/${B}/${OS_VERSION}"
 echo "::set-output name=doc_dir::/private/metwork_addons/${CI}/docs/${B}/mfextaddon_soprano"
+if [ "${CI}" == "continuous_integration" ]; then
+  case "${B}" in
+    master | release_*)
+      echo "::set-output name=nexus_dir::https://artifacts.cloudmf.dev/repository/rpm-snapshots-metwork-addons/${B}/${OS_VERSION}/";;
+    *)
+      echo "::set-output name=nexus_dir::null";;
+  esac
+else
+      echo "::set-output name=nexus_dir::https://artifacts.cloudmf.dev/repository/rpm-releases-metwork-addons/${B}/${OS_VERSION}/";
+fi
