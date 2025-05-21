bd9f60ccb2e2b13979180e2b39cf6598a4b41ea0
metworkbot <metworkbot@metwork-framework.org>
2025-05-21 22:00:54 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -21,6 +21,7 @@ rm -rf html_doc rpms .build_hash
     yum install -y metwork-mfext-layer-radartools-${BRANCH##release_}
     yum install -y metwork-mfext-layer-python3_radartools-${BRANCH##release_}
     yum install -y metwork-mfext-layer-python3_ia-${BRANCH##release_}
+    yum install -y metwork-mfext-layer-python3_extratools-${BRANCH##release_}
 
 
 
