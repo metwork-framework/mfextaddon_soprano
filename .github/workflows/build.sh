fbf06a74bebd637a863c47a0a01e38c1180ccebf
metworkbot <metworkbot@metwork-framework.org>
2026-09-11 14:32:43 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -19,10 +19,10 @@ rm -rf html_doc rpms .build_hash
 
 case "${BRANCH}" in
     ci* | pci*)
-	export DEP_BRANCH=integration
+        export DEP_BRANCH=integration
     *)
-	export DEP_BRANCH=${BRANCH}
-esac;;
+        export DEP_BRANCH=${BRANCH}
+esac
 
     yum install -y metwork-mfext-layer-radartools-${DEP_BRANCH##release_}
     yum install -y metwork-mfext-layer-python3_radartools-${DEP_BRANCH##release_}
